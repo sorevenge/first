6d63a302e849377d4544d9581b1f0d4e01f13d71
wanghanbin <262253059@qq.com>
2015-01-06 17:48:23 +0800

    Initial Commit

---
+++ IOS Combat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOS Combat
+//
+//  Created by wanghanbin on 15/1/6.
+//  Copyright (c) 2015年 wanghanbin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IOS Combat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOS Combat
+//
+//  Created by wanghanbin on 15/1/6.
+//  Copyright (c) 2015年 wanghanbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
